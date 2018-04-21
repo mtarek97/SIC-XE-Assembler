0f4b9112e13a96ebc5836ddb95ada0cefa5be36d
ma7moud_tarek97 <ma7moud.tarek97@gmail.com>
2018-04-21 21:13:04 +0200

    initialize project

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    /*
+    // parsing the source file
+    SourceProgram source = new SourceProgram("example.txt");
+    // generate assembly listing and  object program
+    source.assemble();
+
+    */
+    cout << "Hello Assembler!" << endl;
+    return 0;
+}
