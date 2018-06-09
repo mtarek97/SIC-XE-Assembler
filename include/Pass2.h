3976d485080752b12d729d5cd6f65c839e772bee
MohamedEsmailMohamed <mesmail65525@gmail.com>
2018-06-09 02:18:37 +0200

    pass2 but it is not complete - Esmail

---
+++ include/Pass2.h
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+#include<SourceProgram.h>
+#include<SourceLine.h>
+#include<OpCodeTable.h>
+#include<SyntaxValidator.h>
+#include<AssemblyListing.h>
+#include<SymbolTable.h>
+#include<ValidatorUtilities.h>
+#include<UpdateLocationCounter.h>
+#include <ObjectCodeGenerator.h>
+#include "ObjectProgram.h"
+
+#ifndef PASS2_H
+#define PASS2_H
+
+
+class Pass2
+{
+    public:
+        Pass2(vector<SourceLine> sourceLinesArr,int lengthOfProg);
+        void generateObjProg();
+    protected:
+
+    private:
+        vector<SourceLine> sourceLinesArr;
+        int linesCounter = 0;
+        int lengthOfProg;
+        ObjectProgram objectProgram;
+        ObjectCodeGenerator* objCodeGenerator;
+        string TextRecord;
+        string TextStartAddress;
+        static int MAX_TEXT_RECORED_LENGTH = 60;
+
+        string convertToHEX(int num);
+};
+
+#endif // PASS2_H
--- src/ObjectProgram.cpp
+++ src/ObjectProgram.cpp
@@ -1,8 +1,41 @@
 #include "ObjectProgram.h"
-
+#include<bits/stdc++.h>
+#include<SourceProgram.h>
+#include<SourceLine.h>
+#include<OpCodeTable.h>
+#include<SyntaxValidator.h>
+#include<AssemblyListing.h>
+#include<SymbolTable.h>
+#include<ValidatorUtilities.h>
+#include<UpdateLocationCounter.h>
+#include <ObjectCodeGenerator.h>
+/**
+by Mohamed Esmail
+*/
 ObjectProgram::ObjectProgram()
 {
     //ctor
 }
+void ObjectProgram::writeHeader(string progName, string startingAddress, string lengthOfProg)
+{
+
+}
+
+void ObjectProgram::writeText(string startingAddress, string lengthOFObjCodes, string objCodes)
+{
+
+}
+
+void ObjectProgram::writeEnd(string executionAddress)
+{
+
+}
+
+void ObjectProgram::writeModification(string startingAddress, string Length)
+{
+
+}
+
+
 
 
